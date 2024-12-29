1da33505c3430c4720fe2466a656d00507f76adb
JordiGis <jorgisfer@alu.edu.gva.es>
2024-12-29 19:18:28 +0100

    Initial Commit Refactorizado a Vue

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import { createPinia } from 'pinia';
+import './assets/styles.css'; // Archivo CSS general
+
+const app = createApp(App);
+
+// Configuración de Vue Router y Pinia
+app.use(router);
+app.use(createPinia());
+
+app.mount('#app');
--- src/services/modules.api.js
+++
@@ -1,6 +0,0 @@
-const url = import.meta.env.VITE_API_URL;
-
-export async function getDBModules() {
-  return await fetch(`${url}modules`)
-    .then(response => response.json());
-}
